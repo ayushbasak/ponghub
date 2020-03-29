f5ec0cb537e39d117803f824ac89ff29f27342e9
Ayush Basak <ayushbasak0210@gmail.com>
2020-03-29 19:55:01 +0530

    ponghub v1.0
    The first version of pong hub. 👌
    
    A very basic single player Pong game with a twist.
    
    Library used Processing (p5.js)

---
+++ game.js
@@ -0,0 +1,98 @@
+//jshint esversion:6
+let aud1 = document.getElementById("audio1");
+let aud2 = document.getElementById("audio2");
+let aud3 = document.getElementById("audio3");
+let maxScore = localStorage.getItem("max");
+if(maxScore == null){
+    maxScore = "0";
+    localStorage.setItem("max",maxScore);
+}
+
+function setup(){
+    angleMode(DEGREES);
+    createCanvas(windowWidth,windowHeight);
+
+
+}
+var gameplay = true;
+var hits = 7;
+var initX = 420;
+var initY = 650;
+var x_increment = 10;
+var y_increment = 10;
+var score = 0;
+function draw(){
+        if(initX <= 0)
+            initX = 420;
+
+        background(0);
+
+        strokeWeight(30);
+        stroke(97,108,248);
+        fill(200,200,255);
+        rect(20,mouseY-50,20,100);
+
+        //MAX score
+        if(score > parseInt(maxScore)){
+            maxScore = "" + parseInt(score);
+            localStorage.setItem("max",maxScore);
+        }
+
+        strokeWeight(1);
+        fill(255);
+        stroke(255,255,0);
+        textSize(20);
+        text("m a x s c o r e      :   " + maxScore,1000,100);
+  
+        //SCORE Counter
+        strokeWeight(1);
+        fill(255);
+        stroke(0,255,250);
+        textSize(20);
+        text("s c o r e    :    " + score,1000,140);
+
+
+        initX+= x_increment;
+        initY+= y_increment;
+        arc(initX,initY,20,20,0,360);
+
+        
+        if(Math.abs(initX-60)<=10 &&(initY >= (mouseY-70) && initY <=(mouseY+70))){
+            hits+=1;
+            aud2.play();
+            x_increment = hits;
+            score+=(10*hits);
+        }
+        if(initY<=0){
+            aud1.play();
+            y_increment = hits;
+        }
+        if(initX>=windowWidth){
+            aud1.play();
+            x_increment = -1*hits;
+        }
+        if(initY>=windowHeight){
+            aud1.play();
+            y_increment = -1*hits;
+        }
+        if(initX <= 0)
+        {
+            audio3.play();
+            hits = 7;
+            stroke(200,100,50);
+            stroke(20);
+            fill(250,250,250);
+            textSize(100);
+            text("g  a  m  e  o  v  e  r",(windowWidth/2)-300,400);
+            textSize(20);
+            text("l e f t   c l i c k    t o     r e s t a r t",(windowWidth/2),450);
+            score = 0;
+            noLoop();
+        }
+    
+    
+}
+function mouseClicked() {
+    loop();
+}
+
